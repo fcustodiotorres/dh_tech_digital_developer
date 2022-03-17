9043a8de5b22a33b7b8c244eb1ccf1ef4914bd06
Filipe Custódio <filipe.ctorres@hotmail.com>
2022-03-16 22:51:21 -0300

    Update Front end II

--- 8_front_end_II/aula10_checkpoint/app.js
+++ 8_front_end_II/aula10_checkpoint/app.js
@@ -2,25 +2,20 @@ const nomeReference = document.querySelector("#nome");
 const universoReference = document.querySelector("#universo");
 const descricaoReference = document.querySelector("#descricao");
 const imagemReference = document.querySelector("#urlImagem");
-const buttonReference = document.querySelector("#btnSubmit")
-const heroiListReference = document.querySelector("#heroiList")
-const areaPersonagensReference = document.querySelector("#areaPersonagens")
+const buttonReference = document.querySelector("#btnSubmit");
+const heroiListReference = document.querySelector("#heroiList");
+const areaPersonagensReference = document.querySelector("#areaPersonagens");
 
+buttonReference.addEventListener("click", cadastrarHeroi);
 
-buttonReference.addEventListener('click', cadastrarHeroi)
+function cadastrarHeroi(event) {
+  event.preventDefault();
+  console.log("teste");
 
-function cadastrarHeroi(event){
-    event.preventDefault();
-    console.log('teste')
+  // Capturar os itens dos inputs, ex: nomeReference.value
+  // Inserir os valores capturados dentro do HTML
+
+  areaPersonagensReference.innerHTML += `
+    Adicionar o código HTML aqui usando \${referencia}\
+    `;
 }
-
-/* <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div> */
